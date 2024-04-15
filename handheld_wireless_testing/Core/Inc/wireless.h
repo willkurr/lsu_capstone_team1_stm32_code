26f5669a69b9272010b6621740f0d75a480365bd
CGryder00 <calebgryder@gmail.com>
2024-04-14 22:06:55 -0500

    Added Some Libraries

--- handheld_wireless_testing/Core/Inc/wireless.h
+++ handheld_wireless_testing/Core/Inc/wireless.h
@@ -17,6 +17,33 @@ void Wireless_PowerDown();
 void Wireless_EnableContinuousWave();
 void Wireless_DisableContinuousWave();
 
+/*
+ * Caleb's edits;
+ * change if necessary;
+ * I'm a peasant
+ */
+void Wireless_CONFIG_RegReset();
+void Wireless_ENAA_RegReset();
+void Wireless_ENRXADDR_RegReset();
+void Wireless_SETUPAW_RegReset();
+void Wireless_SETUPRETR_RegReset();
+void Wireless_RFCH_RegReset();
+void Wireless_RFSETUP_RegReset();
+void Wireless_STATUS_RegReset();
+void Wireless_RXPWP0_RegReset();
+void Wireless_RXPWP1_RegReset();
+void Wireless_RXPWP2_RegReset();
+void Wireless_RXPWP3_RegReset();
+void Wireless_RXPWP4_RegReset();
+void Wireless_RXPWP5_RegReset();
+void Wireless_DYNPD_RegReset();
+void Wireless_FEATURE_RegReset();
+void Wireless_TotalRegisterReset();
+uint8_t Wireless_TxHandshake();
+/*
+ * end of Caleb's edits;
+ */
+
 static void CS_High();
 static void CS_Low();
 static void CE_High();
