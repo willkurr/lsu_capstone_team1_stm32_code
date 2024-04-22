c89c81b6412813811f24e43aff8ed06a48a0d664
willkurr <willbill910@gmail.com>
2024-04-22 16:49:57 -0500

    Add progress bar functionality

--- handheld_adc_testing/TouchGFX/gui/src/main_screen/MainView.cpp
+++ handheld_adc_testing/TouchGFX/gui/src/main_screen/MainView.cpp
@@ -15,7 +15,7 @@ void MainView::updateGFXElements() {}
  * @param adcValue The 14-bit adc value to insert
  */
 void MainView::updateADCValue(uint16_t newValue) {
-	boxProgress1.setValue(25);
+	boxProgress1.setValue(100 * newValue/16383);
 	Unicode::snprintf(adcrawvalue_textBuffer,ADCRAWVALUE_TEXT_SIZE,"%d",newValue);
 	adcrawvalue_text.invalidate();
 	adcrawvalue_text.resizeToCurrentText();
